46066f69bdf2c10f2c2791ece02e3f91c885b123
Hass <hassmroueh@gmail.com>
2024-01-15 01:33:52 -0500

    looping code along complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for your kindness!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  function writeCards(names) {
+    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const messages = writeCards(names);
+  
+  console.log(messages);
